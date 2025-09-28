a3ce79d318c2e0c3d1f053dc221f9d90355d6b5a
Neela1004 <neelanithyashree@gmail.com>
2025-09-28 16:24:16 +0530

    Delete Exercise1/Exercise1/behavioral directory

--- Exercise1/Exercise1/behavioral/observer/NewsSubscriber.java
+++
@@ -1,7 +0,0 @@
-package behavioral.observer;
-
-// Interface for any subscriber to NewsAgency
-public interface NewsSubscriber {
-    void update(String news);
-    String getSubscriberName();
-}
--- Exercise1/Exercise1/behavioral/strategy/CardPayment.java
+++
@@ -1,8 +0,0 @@
-package behavioral.strategy;
-
-public class CardPayment implements PaymentStrategy {
-    @Override
-    public void pay(double amount) {
-        System.out.println("Paid " + amount + " using Credit/Debit Card.");
-    }
-}
